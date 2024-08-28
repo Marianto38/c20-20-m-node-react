46a73a61c430fb69ee6d4f154af9f88e82b1799a
Tiziano <tizianorossi508@gmail.com>
2024-08-28 08:39:17 -0300

    First commit tizi

--- Front/src/Screens/HomeScreen/HomeScreen.jsx
+++ Front/src/Screens/HomeScreen/HomeScreen.jsx
@@ -4,7 +4,7 @@ import { NavLink } from 'react-router-dom'
 const HomeScreen = () => {
     return (
         <>
-            <div>HomeScreen</div>
+            <div>HomeScreen hola</div>
             <NavLink to={'/login'}>Login</NavLink>
             <NavLink to={'/sign-up'}>Sign-up</NavLink>
         </>

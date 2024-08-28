f32dc08b83fb2268c00321fdd5fc6c2d447158f5
fhuayller <fhuayller@gmail.com>
2024-08-28 14:21:12 -0300

    professions loaded

---
+++ Back/src/controllers/getProfessions.js
@@ -0,0 +1,14 @@
+const { Professions } = require("../db");
+
+const getProfessions = async (req, res) => {
+  try {
+    const professions = await Professions.findAll();
+
+    return res.status(200).json(professions);
+  } catch (error) {
+    console.error(error);
+    return res.status(500).json({ error: "Error al obtener las profesiones" });
+  }
+};
+
+module.exports = getProfessions;
